44e6a591499d4c42f2c215fa67fcde273c9ea110
Alex Reisner <1683937+reisnera@users.noreply.github.com>
2021-03-22 11:58:49 -0500

    Change parens to curly braces as per common style

--- src/utils.rs
+++ src/utils.rs
@@ -3,10 +3,10 @@ use bitvec::prelude::*;
 // Helper macros
 
 macro_rules! concat_to_c_str {
-    ( $($s:expr),+ ) => ( {
+    ( $($s:expr),+ ) => { {
         use std::os::raw::c_char;
         concat!($($s),+, "\0").as_ptr() as *const c_char
-    } );
+    } };
 }
 
 // Extension traits
