028af1d5495253393bdae178809765fd30eedfb2
Alex Reisner <1683937+reisnera@users.noreply.github.com>
2021-06-18 11:55:21 -0500

    Disambiguate use path in macro

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,7 @@ use bitvec::prelude::*;
 
 macro_rules! concat_to_c_str {
     ( $($s:expr),+ ) => { {
-        use std::os::raw::c_char;
+        use ::std::os::raw::c_char;
         concat!($($s),+, "\0").as_ptr() as *const c_char
     } };
 }
