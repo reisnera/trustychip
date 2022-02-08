5696f71089b19c4b8d8a16c6caa466373c7ea498
Alex Reisner <1683937+reisnera@users.noreply.github.com>
2022-02-08 12:07:01 -0600

    Implement/activate chip8 sound
    - When ST register is non-zero, buzzer sounds.
    - Tested with some ch8 roms and it works!

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -59,11 +59,10 @@ impl<const N: usize> DerefMut for AudioBuffer<N> {
     }
 }
 
-type FrameAudioBuffer = AudioBuffer<{ AUDIO_FRAMES_PER_VIDEO_FRAME * 2 }>;
+type VidFrameAudioBuffer = AudioBuffer<{ AUDIO_FRAMES_PER_VIDEO_FRAME * 2 }>;
 
-// fn generate_audio_sample_batch() -> &'static [i16] {
-fn generate_audio_sample_batch() -> MutexGuard<'static, Box<FrameAudioBuffer>> {
-    static AUDIO_BUFFER: Lazy<Mutex<Box<FrameAudioBuffer>>> =
+fn generate_audio_sample_batch() -> MutexGuard<'static, Box<VidFrameAudioBuffer>> {
+    static AUDIO_BUFFER: Lazy<Mutex<Box<VidFrameAudioBuffer>>> =
         Lazy::new(|| Mutex::new(Box::new(Default::default())));
     static STEP: Mutex<usize> = const_mutex(0);
 
@@ -99,6 +98,12 @@ pub fn run() {
     let user_input = cb::get_input_states();
 
     state::with_mut(|emustate| {
+        if emustate.st > 0 {
+            let buffer_guard = generate_audio_sample_batch();
+            assert_eq!(buffer_guard.len(), AUDIO_FRAMES_PER_VIDEO_FRAME * 2);
+            cb::audio_sample_batch(buffer_guard.as_slice());
+        }
+
         for _ in 0..TIMER_CYCLES_PER_FRAME {
             for _ in 0..TICKS_PER_TIMER_CYCLE {
                 emustate.tick(user_input.as_bitslice());
@@ -109,11 +114,4 @@ pub fn run() {
         }
         cb::video_refresh(&emustate.screen);
     });
-
-    // let len = BUZZER_SAMPLES.as_slice().len();
-    // assert_eq!(len, AUDIO_FRAMES_PER_VIDEO_FRAME * 2);
-    // cb::audio_sample_batch(BUZZER_SAMPLES.as_slice());
-    let buffer_guard = generate_audio_sample_batch();
-    assert_eq!(buffer_guard.len(), AUDIO_FRAMES_PER_VIDEO_FRAME * 2);
-    cb::audio_sample_batch(buffer_guard.as_slice());
 }
